fb777f3a8dd3691c139ec80cdc311b3bf52d3dc2
Andrey Mikheychik <a.mikheychik@gmail.com>
2021-06-06 03:54:40 -0400

    Add shareable Prettier configuration

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const babel = require('./babel');
 const gulp = require('./gulp');
+const prettier = require('./prettier');
 
 exports.babel = babel;
 exports.gulp = gulp;
+exports.prettier = prettier;
